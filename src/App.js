e3548922900b2f3843b8425a1453b0b74676edf9
KE9SH1N <K3NSHI9@gmail.com>
2024-02-15 11:25:47 +0600

    setup react and tailwind files with config tailwind file and create data jason file.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" font-Roboto items-center text-tertiaryText ">
+      <h1>ঘরের বাজার বিডি Ghorer Bazar BD</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+
+    screens:{
+      'xs': '375px',
+      // => @media (min-width: 320px) { ... }
+
+      'sm': '425px',
+      // => @media (min-width: 576px) { ... }
+
+      // 'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1280px) { ... }
+
+      // '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    container:{
+      padding:{
+        'xs':'1rem',
+        'sm': '2rem',
+        'md': '2.75rem',
+        'lg': '2rem',
+        'xl': '1rem',
+      },
+    },
+    colors:{
+      'navBg':'#f3f3f3',
+      'footerBg':'#1e1e1e',
+      'borderLine':'#e7e7e7',
+      'borderCard':'#e4e4e4',
+      'primaryText':'#333333',
+      'secondaryText':'#ffffff',
+      'tertiaryText':'#ff7400',
+      'sectionheadText':'#242424',
+      'placeholderText':'#777777',
+      'priceText':'#ff8f3a',
+      'subtextBg':'#fef3e9',
+      'cardtagBg':'#fef3e9',
+      'cardprimaryBg':'#5cbf3b',
+      'cardtagText':'#ffffff',
+      'buttonBg':'#f68821',
+      'buttonhoverBg':'#221305',
+      'footermenuText':'#a5a5a5',
+      'footerBorder':'#404040',
+      'footerSocialicon':'#ff6700',
+      
+    },
+    fontFamily:{
+      Bangali:['Noto Sans Bengali', 'sans-serif'],
+      Roboto:['Roboto', 'sans-serif'],
+    },
+    
+    extend: {},
+  },
+  plugins: [],
+}
+
